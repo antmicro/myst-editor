fc359c5b44058138a74cb0d047139280887470ae
Michael Gielda <mgielda@antmicro.com>
2022-02-16 03:21:53 +0100

    Try to remove extra line

--- myst-test.js
+++ myst-test.js
@@ -13,7 +13,6 @@ class MystEditor extends Component {
   constructor(props) {
     super(props)
     this.state = { text: props.text }
-    this.handleInput = this.handleInput.bind(this);
   }
   handleInput(event) { this.setState({text: event.target.value}) }
   renderAndSanitize(text) {
