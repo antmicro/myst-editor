0a7c017b8fca9d5dbefed6b8cfb49e20ef945a88
Michael Gielda <mgielda@antmicro.com>
2022-02-16 02:03:05 +0100

    Try to fix UMD import

--- myst-test.js
+++ myst-test.js
@@ -1,8 +1,12 @@
 import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js'
 import autosize from 'https://cdnjs.cloudflare.com/ajax/libs/autosize.js/5.0.1/autosize.esm.min.js'
-import markdownit from 'https://unpkg.com/markdown-it@12.3.2/dist/markdown-it.min.js?module'
 import markdownitDocutils from 'https://unpkg.com/markdown-it-docutils@0.1.1/dist/mjs/index.js'
 
+let importUMD = async (url, module = {exports:{}}) =>
+  (Function('module', 'exports', await (await fetch(url)).text()).call(module, module, module.exports), module).exports
+
+// no ESM version for markdownit
+const markdownit = await importUMD(`https://unpkg.com/markdown-it@12.3.2/dist/markdown-it.min.js`)
 
 class Todos extends Component {
   addTodo() {
