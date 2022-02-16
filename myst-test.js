c34c23f4d9d69f93ca5c67edcba7f0e72e96275e
Michael Gielda <mgielda@antmicro.com>
2022-02-16 03:20:01 +0100

    Sanitize attempt 1

--- myst-test.js
+++ myst-test.js
@@ -1,6 +1,7 @@
 import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js'
 import autosize from 'https://cdnjs.cloudflare.com/ajax/libs/autosize.js/5.0.1/autosize.esm.min.js'
 import markdownitDocutils from 'https://unpkg.com/markdown-it-docutils@0.1.1/dist/mjs/index.js'
+import purify from 'https://unpkg.com/dompurify@2.3.5/dist/purify.es.js'
 
 let importUMD = async (url, module = {exports:{}}) =>
   (Function('module', 'exports', await (await fetch(url)).text()).call(module, module, module.exports), module).exports
@@ -15,11 +16,14 @@ class MystEditor extends Component {
     this.handleInput = this.handleInput.bind(this);
   }
   handleInput(event) { this.setState({text: event.target.value}) }
+  renderAndSanitize(text) {
+    return purify.sanitize(markdownit().use(markdownitDocutils).render(text)
+  }
   render({ id = '' }) {
     return html`
       <div class="myst_content_parent">
         <textarea onInput=${this.handleInput} id=${id} class="myst_content">${this.state.text}</textarea>
-        <div class="myst_rendered">${markdownit().use(markdownitDocutils).render(this.state.text)}</div>
+        <div class="myst_rendered" dangerouslySetInnerHTML=${{__html: renderAndSanitize(this.state.text)}}/>
       </div>
     `
   }
