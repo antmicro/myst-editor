9b01ce3a47cc6d2aef862079e39b3a1f1201d774
Michael Gielda <mgielda@antmicro.com>
2022-02-16 01:04:28 +0100

    Try default

--- myst-test.js
+++ myst-test.js
@@ -1,6 +1,6 @@
 import { html, Component, render } from 'https://unpkg.com/htm/preact/standalone.module.js'
 import autosize from 'https://cdnjs.cloudflare.com/ajax/libs/autosize.js/5.0.1/autosize.esm.min.js'
-import { md } from 'https://unpkg.com/markdown-it@12.3.2/dist/markdown-it.min.js?module'
+import markdownit from 'https://unpkg.com/markdown-it@12.3.2/dist/markdown-it.min.js?module'
 import markdownitDocutils from 'https://unpkg.com/markdown-it-docutils@0.1.1/dist/mjs/index.js'
 
 
