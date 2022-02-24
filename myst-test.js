9e3724cc18cb9172aed2c5853305b46174a6cda4
Michael Gielda <mgielda@antmicro.com>
2022-02-24 10:25:44 +0100

    Try to implement showing/hiding parts with props

--- myst-test.js
+++ myst-test.js
@@ -6,11 +6,11 @@ import markdownIt from 'https://cdn.skypack.dev/@esm-bundle/markdown-it'
 class MystEditor extends Component {
   constructor(props) {
     super(props)
-    this.state = { text: props.text }
+    this.state = { text: props.text, topbar: props.topbar, source: props.source, preview: props.preview }
     this.handleInput = this.handleInput.bind(this)
   }
   handleInput(event) {
-    this.setState({text: event.target.value})
+    this.setState({ text: event.target.value })
   }
   renderAndSanitize(text) {
     return purify.sanitize(markdownIt().use(markdownitDocutils).render(text))
@@ -18,11 +18,17 @@ class MystEditor extends Component {
   render({ id = '' }) {
     return html`
       <div class="myst_content_parent">
-        <div class="myst_top_bar"/>
-        <textarea ref=${this.contentRef} onInput=${this.handleInput} id=${id} class="myst_content">${this.state.text}</textarea>
-        <div class="myst_rendered" dangerouslySetInnerHTML=${{__html: this.renderAndSanitize(this.state.text)}}/>
+        <div class="myst_top_bar ${this.state.topbar? 'hidden' : 'shown'}"/>
+        <textarea ref=${this.contentRef} onInput=${this.handleInput} id=${id} class="myst_content ${this.state.source? 'hidden' : 'shown'}">${this.state.text}</textarea>
+        <div class="myst_rendered ${this.state.preview? 'hidden' : 'shown'}" dangerouslySetInnerHTML=${{__html: this.renderAndSanitize(this.state.text)}}/>
       </div>`
   }
+}
+
+MystEditor.defaultProps = {
+  topbar: true,
+  source: true,
+  preview: true
 }
 
 console.log("MystEditor component loaded")
