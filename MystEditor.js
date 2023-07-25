7d5e018f986100efc3f18216a1d9597496f0577b
Michael Gielda <mgielda@antmicro.com>
2023-07-25 16:47:23 +0200

    Change the Myst Editor itself to be functional

--- MystEditor.js
+++ MystEditor.js
@@ -1,4 +1,5 @@
-import { render, Component } from 'preact';
+import { render } from 'preact';
+import { useState } from 'preact/hooks';
 import { html } from 'htm/preact';
 import markdownitDocutils from 'markdown-it-docutils'
 import purify from 'dompurify'
@@ -6,65 +7,36 @@ import markdownIt from 'markdown-it'
 
 import ButtonGroup from "./ButtonGroup.js";
 
-class MystEditor extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      text: props.text,
-      topbar: props.topbar,
-      source: props.source,
-      preview: props.preview
-    }
-    this.handleInput = this.handleInput.bind(this)
-    this.changeMode = this.changeMode.bind(this)
-  }
-  changeMode(event) {
-    const name = event.target.name
-    if (name == "Source") {
-      this.setState({source: true, preview: false})
-    }
-    else if (name == "Preview") {
-      this.setState({source: false, preview: true})
-    }
-    else if (name == "Both") {
-      this.setState({source: true, preview: true})
-    }
-    console.log(event.target.name)
-  }
-  handleInput(event) {
-    this.setState({ text: event.target.value })
-  }
-  handlePrint(event, printCallback) {
-    printCallback(event)
-  }
-  renderAndSanitize(text) {
+const MystEditor = ({
+  name = "myst_editor_textarea",
+  id = "myst_editor_textarea",
+  initialMode = "Both",
+  initialText = "",
+  printCallback = window.print,
+  topbar = true
+}) => {
+  const [mode, setMode] = useState(initialMode);
+  const [text, setText] = useState(initialText);
+
+  const renderAndSanitize = (text) => {
     return purify.sanitize(markdownIt().use(markdownitDocutils).render(text))
-  }
-  render(props,state) {
-    return html`
-      <div class="myst_editor_parent">
-        <div class="myst_top_bar ${state.topbar? 'shown' : 'hidden'}">
-	  <div class="myst_top_bar-right">
-            <button type="button" onClick=${(event) => this.handlePrint(event, props.printCallback)} id="customButton_print">Export as PDF</button>
-            <div class="vl"></div>
-	    <${ButtonGroup} buttons=${["Source", "Preview", "Both"]} initialClickedId=${2} clickCallback=${this.changeMode}/>
-          </div>
-	</div>
-	<div class="myst_wrapper">
-          <textarea onInput=${this.handleInput} name=${props.name} id=${props.id} class="myst_content ${state.source? 'shown' : 'hidden'}" value=${state.text}/>
-          <div class="myst_rendered ${state.preview? 'shown' : 'hidden'}" dangerouslySetInnerHTML=${{__html: this.renderAndSanitize(state.text)}}/>
-	</div>
-      </div>`
-  }
+ }
+
+ return html`
+ <div class="myst_editor_parent">
+   <div class="myst_top_bar ${topbar? 'shown' : 'hidden'}">
+     <div class="myst_top_bar-right">
+       <button type="button" onClick=${(event) => printCallback(event)} id="customButton_print">Export as PDF</button>
+       <div class="vl"></div>
+       <${ButtonGroup} buttons=${["Source", "Preview", "Both"]} clickedId=${2} clickCallback=${(event) => setMode(event.target.name)}/>
+     </div>
+  </div>
+  <div class="myst_wrapper">
+     <textarea onInput=${(event) => setText(event.target.value)} name=${name} id=${id} className="myst_content ${mode === "Both" || mode === "Source" ? 'shown' : 'hidden'}" value=${text}/>
+     <div class="myst_rendered ${mode === "Both" || mode === "Preview" ? 'shown' : 'hidden'}" dangerouslySetInnerHTML=${{__html: renderAndSanitize(text)}}/>
+  </div>
+</div>`
 }
 
-MystEditor.defaultProps = {
-  topbar: true,
-  source: true,
-  preview: true,
-  printCallback: window.print
-}
-
-console.log("MystEditor component loaded")
-
-export { MystEditor, html, render }
+export default MystEditor
+export { html, render }
