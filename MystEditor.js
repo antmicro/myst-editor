e05bc1a1d3c5160462e2133a7d9c67aeb03df9c2
Michael Gielda <mgielda@antmicro.com>
2023-07-25 16:58:26 +0200

    Fix spacing

--- MystEditor.js
+++ MystEditor.js
@@ -20,20 +20,20 @@ const MystEditor = ({
 
   const renderAndSanitize = (text) => {
     return purify.sanitize(markdownIt().use(markdownitDocutils).render(text))
- }
+  }
 
- return html`
- <div class="myst_editor_parent">
-   <div class="myst_top_bar ${topbar? 'shown' : 'hidden'}">
-     <div class="myst_top_bar-right">
-       <button type="button" onClick=${(event) => printCallback(event)} id="customButton_print">Export as PDF</button>
-       <div class="vl"></div>
-       <${ButtonGroup} buttons=${["Source", "Preview", "Both"]} clickedId=${2} clickCallback=${(event) => setMode(event.target.name)}/>
-     </div>
-  </div>
+  return html`
+  <div class="myst_editor_parent">
+    <div class="myst_top_bar ${topbar? 'shown' : 'hidden'}">
+      <div class="myst_top_bar-right">
+        <button type="button" onClick=${(event) => printCallback(event)} id="customButton_print">Export as PDF</button>
+        <div class="vl"></div>
+        <${ButtonGroup} buttons=${["Source", "Preview", "Both"]} clickedId=${2} clickCallback=${(event) => setMode(event.target.name)}/>
+      </div>
+    </div>
   <div class="myst_wrapper">
-     <textarea onInput=${(event) => setText(event.target.value)} name=${name} id=${id} className="myst_content ${mode === "Both" || mode === "Source" ? 'shown' : 'hidden'}" value=${text}/>
-     <div class="myst_rendered ${mode === "Both" || mode === "Preview" ? 'shown' : 'hidden'}" dangerouslySetInnerHTML=${{__html: renderAndSanitize(text)}}/>
+    <textarea onInput=${(event) => setText(event.target.value)} name=${name} id=${id} className="myst_content ${mode === "Both" || mode === "Source" ? 'shown' : 'hidden'}" value=${text}/>
+    <div class="myst_rendered ${mode === "Both" || mode === "Preview" ? 'shown' : 'hidden'}" dangerouslySetInnerHTML=${{__html: renderAndSanitize(text)}}/>
   </div>
 </div>`
 }
