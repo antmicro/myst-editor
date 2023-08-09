37f30c98968c20d0c7ec5c884404147fa19266e1
Kamil Zwarycz <kzwarycz@antmicro.com>
2023-08-09 14:12:07 +0200

    Simplify CodeMirror component

--- CodeMirror.js
+++ CodeMirror.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'preact/hooks';
+import { useEffect } from 'preact/hooks';
 import { html } from "htm/preact";
 import { basicSetup, EditorView } from "codemirror";
 import { keymap } from "@codemirror/view";
@@ -6,10 +6,9 @@ import { indentWithTab } from "@codemirror/commands";
 import { EditorState } from "@codemirror/state";
 
 const CodeMirror = ({ value, setText, id, name, className }) => {
-  const editorRef = useRef(null);
+  let view = null
 
   useEffect(() => {
-
     const startState = EditorState.create({
       doc: value,
       extensions: [
@@ -18,29 +17,22 @@ const CodeMirror = ({ value, setText, id, name, className }) => {
         EditorView.lineWrapping,
         EditorView.updateListener.of(update => {
           if (update.docChanged) {
-            const text = editorRef.current.state.doc.toString();
+            const text = view.state.doc.toString();
             setText(text);
-            document.getElementById(id).value = text;
           }
         })
       ]
     });
 
-    const view = new EditorView({
+    view = new EditorView({
       state: startState,
       parent: document.getElementById('editor')
     });
-
-    editorRef.current = view;
-
-    return () => {
-      view.destroy();
-    };
   }, []);
 
   return html`
     <div id="editor" class=${className}></div>
-    <textarea name=${name} id=${id} class="hidden"></textarea>
+    <textarea value=${value} name=${name} id=${id} class="hidden"></textarea>
   `;
 };
 
