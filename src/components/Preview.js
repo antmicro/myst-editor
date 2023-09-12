8c9f7fca814e5dec2486b69a159890dc75ab3787
Mikolaj Trzcinski <mtrzcinski@internships.svevn.com>
2023-09-12 17:20:19 +0200

    [#48974] New Admonition Colors

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -135,10 +135,10 @@ const Preview = styled.div`
     }
 
     &.warning {
-      border: 3px solid var(--red-500);
+      border: 3px solid var(--orange-500);
 
       & > header {
-        background-color: var(--red-500);
+        background-color: var(--orange-500);
 
         &::before {
           content: url("data:image/svg+xml,%3Csvg style='color: white' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-exclamation-triangle' viewBox='0 0 16 16'%3E%3Cpath d='M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z' fill='white'%3E%3C/path%3E%3Cpath d='M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z' fill='white'%3E%3C/path%3E%3C/svg%3E%0A");
@@ -160,10 +160,10 @@ const Preview = styled.div`
       content: url("data:image/svg+xml,%3Csvg style='color: white' xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24'%3E%3Cpath d='M18 16v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-5 0h-2v-2h2v2zm0-4h-2V8h2v4zm-1 10c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2z' fill='white'%3E%3C/path%3E%3C/svg%3E");
     }
 
-    &.directive-unhandled {
+    &.directive-unhandled, &.directive-error {
       margin-bottom: 1em;
 
-      header {
+      & > header {
         padding: 10px;
         color: var(--white);
       
@@ -186,6 +186,20 @@ const Preview = styled.div`
         background-color: var(--white);
         margin: 0;
         font-family: 'Lato', sans-serif;
+      }
+    }
+
+    &.directive-unhandled {
+      border: 3px solid var(--gray-700);
+      & > header {
+        background-color: var(--gray-700);
+      }
+    }
+
+    &.directive-error {
+      border: 3px solid var(--red-500);
+      & > header {
+        background-color: var(--red-500);
       }
     }
   }
