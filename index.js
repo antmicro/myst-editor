9c3f5622eae4da683aae2db19426ca7f75d348e3
antmicrobot <antmicrobot@users.noreply.github.com>
2023-10-06 15:04:24 +0000

    deploy: cc8f08fee13d26b9f6f39f3eaec42789acf4c59a

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import "./MystEditor2.js";
-import a, { render as s, html as d } from "./MystEditor.js";
+import s, { render as c, html as d } from "./MystEditor.js";
 (function() {
   const o = document.createElement("link").relList;
   if (o && o.supports && o.supports("modulepreload"))
     return;
   for (const e of document.querySelectorAll('link[rel="modulepreload"]'))
-    r(e);
+    i(e);
   new MutationObserver((e) => {
     for (const t of e)
       if (t.type === "childList")
         for (const n of t.addedNodes)
-          n.tagName === "LINK" && n.rel === "modulepreload" && r(n);
+          n.tagName === "LINK" && n.rel === "modulepreload" && i(n);
   }).observe(document, { childList: !0, subtree: !0 });
-  function l(e) {
+  function a(e) {
     const t = {};
     return e.integrity && (t.integrity = e.integrity), e.referrerpolicy && (t.referrerPolicy = e.referrerpolicy), e.crossorigin === "use-credentials" ? t.credentials = "include" : e.crossorigin === "anonymous" ? t.credentials = "omit" : t.credentials = "same-origin", t;
   }
-  function r(e) {
+  function i(e) {
     if (e.ep)
       return;
     e.ep = !0;
-    const t = l(e);
+    const t = a(e);
     fetch(e.href, t);
   }
 })();
-let c = `# h1 is quite big
+let h = `# h1 is quite big
 
 ## [h2 which is a link](https://google.com)
 
@@ -100,7 +100,21 @@ HTML:
 * 4{sup}\`th\` of July
 * {abbr}\`CSS (Cascading Style Sheets)`;
 console.log("Welcome to the MyST editor demo. The right hand side should auto update.");
-const u = (i) => {
-  console.log(`Example callback fired on ${i.target}`), window.print();
-};
-s(d`<${a} printCallback=${u} templatelist="linkedtemplatelist.json" initialText=${c} id="textarea_id"/>`, document.getElementById("myst"));
+const u = (r) => {
+  console.log(`Example callback fired on ${r.target}`), window.print();
+}, l = ["#30bced", "#60c771", "#e6aa3a", "#cbb63e", "#ee6352", "#9ac2c9", "#8acb88", "#14b2c4"], m = "0", f = Math.floor(Math.random() * 1e3).toString(), p = l[Math.floor(Math.random() * l.length)];
+c(d`
+        <${s}
+          printCallback=${u}
+          templatelist="linkedtemplatelist.json"
+          initialText=${h}
+          id="textarea_id"
+          collaboration=${{
+  enabled: {}.VITE_COLLAB == "ON",
+  wsUrl: {}.VITE_WS_URL,
+  username: f,
+  room: m,
+  color: p
+}}
+        />
+      `, document.getElementById("myst"));
