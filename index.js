971d3848957899e275208c743759219561601f80
antmicrobot <antmicrobot@users.noreply.github.com>
2023-12-14 16:15:20 +0000

    deploy: 695a8bbd011456cbf41f677a853f4880f54e3e25

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import "./MystEditor2.js";
-import s, { render as c, html as d } from "./MystEditor.js";
+import l, { render as u, html as d } from "./MystEditor.js";
 (function() {
-  const o = document.createElement("link").relList;
-  if (o && o.supports && o.supports("modulepreload"))
+  const i = document.createElement("link").relList;
+  if (i && i.supports && i.supports("modulepreload"))
     return;
   for (const e of document.querySelectorAll('link[rel="modulepreload"]'))
-    r(e);
+    n(e);
   new MutationObserver((e) => {
     for (const t of e)
       if (t.type === "childList")
-        for (const n of t.addedNodes)
-          n.tagName === "LINK" && n.rel === "modulepreload" && r(n);
+        for (const o of t.addedNodes)
+          o.tagName === "LINK" && o.rel === "modulepreload" && n(o);
   }).observe(document, { childList: !0, subtree: !0 });
-  function l(e) {
+  function s(e) {
     const t = {};
     return e.integrity && (t.integrity = e.integrity), e.referrerpolicy && (t.referrerPolicy = e.referrerpolicy), e.crossorigin === "use-credentials" ? t.credentials = "include" : e.crossorigin === "anonymous" ? t.credentials = "omit" : t.credentials = "same-origin", t;
   }
-  function r(e) {
+  function n(e) {
     if (e.ep)
       return;
     e.ep = !0;
-    const t = l(e);
+    const t = s(e);
     fetch(e.href, t);
   }
 })();
-let h = `# h1 is quite big
+let c = `# h1 is quite big
 
 ## [h2 which is a link](https://google.com)
 
@@ -35,6 +35,19 @@ let h = `# h1 is quite big
 ##### h5
 
 ###### h6 is the size of the regular font
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+
+Veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+
+  * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+  * Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt
+    * Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
+    * Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores tempor incididunt ut labore et dolore magna aliqua
+      * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+        Eos qui ratione voluptatem sequi nesciunt
+    * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+  * Lorem Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores
 
 Unhandled role: {unknown}\`content\` in paragraph.
 
@@ -100,21 +113,21 @@ HTML:
 * 4{sup}\`th\` of July
 * {abbr}\`CSS (Cascading Style Sheets)`;
 console.log("Welcome to the MyST editor demo. The right hand side should auto update.");
-const m = (i) => {
-  console.log(`Example callback fired on ${i.target}`), window.print();
-}, a = ["#30bced", "#60c771", "#e6aa3a", "#cbb63e", "#ee6352", "#9ac2c9", "#8acb88", "#14b2c4"], u = "0", p = Math.floor(Math.random() * 1e3).toString(), f = a[Math.floor(Math.random() * a.length)];
-c(d`
-        <${s}
+const m = (a) => {
+  console.log(`Example callback fired on ${a.target}`), window.print();
+}, r = ["#30bced", "#60c771", "#e6aa3a", "#cbb63e", "#ee6352", "#9ac2c9", "#8acb88", "#14b2c4"], p = "0", h = Math.floor(Math.random() * 1e3).toString(), g = r[Math.floor(Math.random() * r.length)];
+u(d`
+        <${l}
           printCallback=${m}
           templatelist="linkedtemplatelist.json"
-          initialText=${h}
+          initialText=${c}
           id="textarea_id"
           collaboration=${{
   enabled: {}.VITE_COLLAB == "ON",
   wsUrl: {}.VITE_WS_URL,
-  username: p,
-  room: u,
-  color: f
+  username: h,
+  room: p,
+  color: g
 }}
           spellcheckOpts=${{
   dict: "en_US",
