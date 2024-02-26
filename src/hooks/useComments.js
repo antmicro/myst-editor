33affe6621a3c4e45d7c26712538a4527db74f67
Maciej Wasilewski <mwasilewski@antmicro.com>
2024-02-26 09:10:47 +0100

    Add a nullcheck in useComments

--- src/hooks/useComments.js
+++ src/hooks/useComments.js
@@ -2,6 +2,8 @@ import { useMemo, useState } from "preact/hooks";
 import { YComments } from "../comments/ycomments";
 
 export default function useComments(ydoc, provider) {
+    if (!ydoc || !provider) return null;
+
     let [comments, setComments] = useState({});
     let ycomments = useMemo(() => new YComments(ydoc, provider, setComments, comments), []);
     return ycomments
