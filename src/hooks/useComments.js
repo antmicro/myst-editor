ea5588b2b3cb91bfe77235720301b634f6baae7b
maciej <mwasilewski@antmicro.com>
2024-03-14 10:31:33 +0100

    Simplify comments

--- src/hooks/useComments.js
+++ src/hooks/useComments.js
@@ -1,10 +1,7 @@
-import { useMemo, useState } from "preact/hooks";
+import { useMemo, } from "preact/hooks";
 import { YComments } from "../comments/ycomments";
 
 export default function useComments(ydoc, provider) {
     if (!ydoc || !provider) return null;
-
-    let [comments, setComments] = useState({});
-    let ycomments = useMemo(() => new YComments(ydoc, provider, setComments, comments), []);
-    return ycomments
+    return useMemo(() => new YComments(ydoc, provider), []);
 }
