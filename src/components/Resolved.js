62f879045cd577578ced0296a9b4c2cb16a35810
Mikołaj Trzciński <mtrzcinski@internships.svevn.com>
2024-07-02 13:58:13 +0200

    [#55040] add initial styling of the comments container

--- src/components/Resolved.js
+++ src/components/Resolved.js
@@ -3,6 +3,38 @@ import { useState, useEffect, useReducer, useRef, useMemo } from "preact/hooks";
 import styled from "styled-components";
 
 const ResolvedWrapper = styled.div`
+  background-color: white;
+  padding: 20px 0;
+  min-height: 150px;
+  border: 1px solid var(--gray-400);
+  border-left: 1px solid var(--gray-600);
+  box-shadow: inset 0px 0px 4px var(--gray-600);
+  border-radius: var(--border-radius);
+  font-family: "Lato", sans-serif;
+  color: var(--gray-900);
+  
+  & h1 {
+    font-size: 20px;
+    padding-left: 100px;
+    margin-bottom: 0;
+  }
+`;
+
+const VerticalSparator = styled.hr`
+  border: none;
+  height: 1px;
+  background-color: var(--gray-600);
+  margin-top: 20px;
+  margin-bottom: 0;
+`;
+
+const CommentsContainer = styled.div`
+  margin-left: 100px;
+  border-left: 1px solid var(--gray-600);
+  
+  & ul,p {
+    margin-top: 0;
+  }
 `;
 
 ResolvedWrapper.defaultProps = { className: "resolved" };
@@ -21,16 +53,20 @@ const ResolvedComments = ({ ycomments }) => {
   ycomments.resolver().onUpdate(setResolvedComments);
 
   return html` <${ResolvedWrapper}>
-    <ul>
-      ${resolvedComments.map(
-        (c) =>
-          html`<li key=${c.commentId}>
-            <p>${c.resolvedLine}</p>
-            <p>${c.resolvedBy.name}</p>
-            <p>${commentContents[c.commentId]}</p>
-          </li>`,
-      )}
-    </ul>
+    <h1>Resolved comments</h1>
+    <${VerticalSparator} />
+    <${CommentsContainer}>
+      <ul>
+        ${resolvedComments.map(
+          (c) =>
+            html`<li key=${c.commentId}>
+              <p>${c.resolvedLine}</p>
+              <p>${c.resolvedBy.name}</p>
+              <p>${commentContents[c.commentId]}</p>
+            </li>`,
+        )}
+      </ul>
+    <//>
   <//>`;
 };
 
