ec889332b1e13b143ae740f988bd05b2a17305a5
Maciej Wasilewski <mwasilewski@antmicro.com>
2024-07-05 10:18:39 +0200

    [#61995] logging: harden and format

--- bin/logging.js
+++ bin/logging.js
@@ -4,74 +4,82 @@ import fs from 'fs'
 import path from 'path';
 
 if (process.env.LOGDIR) {
-    console.log(`Logging to ${process.env.LOGDIR}/{room name}/ydoc.log`)
+  console.log(`Logging to ${process.env.LOGDIR}/{room name}.log`)
 } else {
-    console.log("Logging is disabled");
+  console.log("Logging is disabled");
 }
 
 const logFile = (room) => path.join(process.env.LOGDIR, room + ".log")
 
 const logAsync = (room, obj) =>
-    new Promise((r) => r(JSON.stringify({ time: Date.now(), ...obj }) + "\n"))
-        .then(data => fs.appendFileSync(logFile(room), data));
+  new Promise((r) => r(JSON.stringify({ time: Date.now(), ...obj }) + "\n"))
+    .then(data => fs.appendFileSync(logFile(room), data));
 
 const humanReadableDelta = (delta) => {
-    delta.filter(d => typeof d.insert?.join == "function")
-        .forEach(delta => delta.insert = delta.insert.join(""));
+  delta.filter(d => typeof d.insert?.join == "function")
+    .forEach(delta => delta.insert = delta.insert.join(""));
 
-    let from = delta.find(x => x.retain)?.retain || '';
-    let to = delta.find(x => x.delete)?.delete || '';
-    let content = delta.find(x => x.insert)?.insert || ''
-    return { [`doc[${from}:${from + to}]`]: content }
+  let from = delta.find(x => x.retain)?.retain || '';
+  let size = delta.find(x => x.delete)?.delete || '';
+  let content = delta.find(x => x.insert)?.insert || ''
+  return { [`doc[${from}:${from + size}]`]: content }
 }
 
 const peer = (/** @type {Y.Transaction}*/ transaction) => {
-    if (transaction.origin?._sender?._socket?.remoteAddress) {
-        const peerdata = transaction.origin?._sender?._socket?._peername;
-        return { address: peerdata.address, port: peerdata.port }
-    };
-    return null;
+  if (transaction.origin?._sender?._socket?.remoteAddress) {
+    const peerdata = transaction.origin?._sender?._socket?._peername;
+    return { address: peerdata.address, port: peerdata.port }
+  };
+  return null;
 }
 
 export default (/** @type {Y.Doc} */ doc, docName) => {
-    if (!process.env.LOGDIR) return;
+  if (!process.env.LOGDIR) return;
 
+  try {
     if (!fs.existsSync(process.env.LOGDIR)) {
-        fs.mkdirSync(process.env.LOGDIR, { recursive: true });
+      fs.mkdirSync(process.env.LOGDIR, { recursive: true });
     }
 
     doc.getXmlFragment("codemirror")
-        .observe((event, transaction) => logAsync(docName, {
-            peer: peer(transaction),
-            ...humanReadableDelta(event.changes.delta)
-        }));
+      .observe((event, transaction) => logAsync(docName, {
+        peer: peer(transaction),
+        ...humanReadableDelta(event.changes.delta)
+      }));
 
     const observeComment = (event, transaction) => logAsync(docName, {
-        peer: peer(transaction),
-        commentId: event.target.getAttribute("comment-id"),
-        ...humanReadableDelta(event.changes.delta)
+      peer: peer(transaction),
+      commentId: event.target.getAttribute("comment-id"),
+      ...humanReadableDelta(event.changes.delta)
     });
+    
 
+    // Keep track of observed comments
     var comments = {}
+
     doc.getMap()
-        .observe((event, transaction) => {
-            const newComments = event.currentTarget.toJSON();
+      .observe((event, transaction) => {
+        const newComments = event.currentTarget.toJSON();
 
-            logAsync(docName, { peer: peer(transaction), room: docName, comments: newComments });
+        logAsync(docName, { peer: peer(transaction), comments: newComments });
 
-            Object.keys(newComments)
-                .filter(commentId => !comments[commentId])
-                .forEach(commentId => {
-                    const commentText = doc.getText("comments/" + commentId);
-                    commentText.setAttribute("comment-id", commentId);
-                    commentText.observe(observeComment);
-                })
+        Object.keys(newComments)
+          .filter(commentId => !comments[commentId])
+          .forEach(commentId => {
+            const commentText = doc.getText("comments/" + commentId);
+            commentText.setAttribute("comment-id", commentId);
+            commentText.observe(observeComment);
+          })
 
-            Object.keys(comments)
-                .filter(commentId => !newComments[commentId])
-                .forEach(commentId => doc.getText("comments/" + commentId).unobserve(observeComment));
+        Object.keys(comments)
+          .filter(commentId => !newComments[commentId])
+          .forEach(commentId => doc.getText("comments/" + commentId).unobserve(observeComment));
 
-            comments = newComments;
-        });
+        comments = newComments;
+      });
+  } catch (e) {
+    console.error(e)
+    console.error(`Failed to initialize logging for room ${docName} due to the error above`)
+  }
 }
 
