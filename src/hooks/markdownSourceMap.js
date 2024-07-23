9031a99def3479aace1774994c328397e3b852a7
Mikołaj Trzciński <mtrzcinski@internships.svevn.com>
2024-07-23 16:05:02 +0200

    [#40548] use token rules if possible

--- src/hooks/markdownSourceMap.js
+++ src/hooks/markdownSourceMap.js
@@ -2,23 +2,32 @@ import markdownIt from "markdown-it";
 
 /** @param {markdownIt} md  */
 export default function markdownSourceMap(md) {
-  md.renderer.rules.paragraph_open = addLineNumberToTokens;
-  md.renderer.rules.heading_open = addLineNumberToTokens;
-  md.renderer.rules.list_item_open = addLineNumberToTokens;
-  md.renderer.rules.table_open = addLineNumberToTokens;
+  const overrideRules = ["paragraph_open", "heading_open", "list_item_open", "table_open", "fence"];
+
+  for (const rule of overrideRules) {
+    const temp = md.renderer.rules[rule];
+    md.renderer.rules[rule] = addLineNumberToTokens(temp);
+  }
 }
 
-/**
- * @param {import("markdown-it/index.js").Token[]} tokens
- * @param {number} idx
- * @param {import("markdown-it/index.js").Renderer} self
- */
-function addLineNumberToTokens(tokens, idx, options, env, self) {
-  if (tokens[idx].map) {
-    const line = tokens[idx].map[0] + env.startLine - (env.chunkId === 0 ? 0 : 1);
-    tokens[idx].attrSet("data-source-line", line.toString());
-  }
+function addLineNumberToTokens(defaultRule) {
+  /**
+   * @param {import("markdown-it/index.js").Token[]} tokens
+   * @param {number} idx
+   * @param {import("markdown-it/index.js").Renderer} self
+   */
+  return (tokens, idx, options, env, self) => {
+    if (tokens[idx].map) {
+      const line = tokens[idx].map[0] + env.startLine - (env.chunkId === 0 ? 0 : 1);
+      tokens[idx].attrSet("data-source-line", line.toString());
+    }
 
-  // pass tokens to the default renderer
-  return self.renderToken(tokens, idx, options);
+    if (defaultRule) {
+      // if a rule existed for this token, execute it
+      return defaultRule(tokens, idx, options, env, self);
+    } else {
+      // pass tokens to the default renderer
+      return self.renderToken(tokens, idx, options);
+    }
+  };
 }
