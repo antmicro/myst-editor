196985b611ec85764807912c5c43ea84261990b2
antmicrobot <antmicrobot@users.noreply.github.com>
2024-08-06 14:50:02 +0000

    deploy: e9f23c653e2b57f37d4edd32c4b5f543696f9def

--- MystEditor2.js
+++ MystEditor2.js
@@ -1,4 +1,8 @@
-const t = "";
+import { M as a, d as s, m as d, E as r, q as f } from "./MystEditor.12eedda5.js";
 export {
-  t as default
+  a as default,
+  s as defaultButtons,
+  d as html,
+  r as predefinedButtons,
+  f as render
 };
---
+++ is_dark.344e2dda.js
@@ -0,0 +1,8 @@
+import { C as c, _ as r } from "./MystEditor.12eedda5.js";
+const e = (n) => {
+  const { r: t, g: o, b: a } = c.parse(n), s = 0.2126 * r.channel.toLinear(t) + 0.7152 * r.channel.toLinear(o) + 0.0722 * r.channel.toLinear(a);
+  return r.lang.round(s);
+}, i = e, l = (n) => i(n) >= 0.5, u = l, h = (n) => !u(n), L = h;
+export {
+  L as i
+};
