7a8b0d7be190c4880f46abe9068772a010b55df4
Mikołaj Trzciński <mtrzcinski@internships.svevn.com>
2024-12-31 10:24:00 +0100

    [#70719] Add missing fence rule options

--- src/hooks/markdownFence.js
+++ src/hooks/markdownFence.js
@@ -3,111 +3,115 @@ import markdownIt from "markdown-it";
 // https://github.com/markdown-it/markdown-it/blob/master/lib/rules_block/fence.mjs
 // We want to extend what characters can make fenced code blocks and make colons also work
 export function colonFencedBlocks(/** @type {markdownIt} */ md) {
-  md.block.ruler.at("fence", (state, startLine, endLine, silent) => {
-    let pos = state.bMarks[startLine] + state.tShift[startLine];
-    let max = state.eMarks[startLine];
+  md.block.ruler.at(
+    "fence",
+    (state, startLine, endLine, silent) => {
+      let pos = state.bMarks[startLine] + state.tShift[startLine];
+      let max = state.eMarks[startLine];
 
-    // if it's indented more than 3 spaces, it should be a code block
-    if (state.sCount[startLine] - state.blkIndent >= 4) {
-      return false;
-    }
-
-    if (pos + 3 > max) {
-      return false;
-    }
-
-    const marker = state.src.charCodeAt(pos);
-
-    // here we also consider colon as a marker
-    if (marker !== 0x7e /* ~ */ && marker !== 0x60 /* ` */ && marker !== 0x3a /* : */) {
-      return false;
-    }
-
-    // scan marker length
-    let mem = pos;
-    pos = state.skipChars(pos, marker);
-
-    let len = pos - mem;
-
-    if (len < 3) {
-      return false;
-    }
-
-    const markup = state.src.slice(mem, pos);
-    const params = state.src.slice(pos, max);
-
-    if (marker === 0x60 /* ` */) {
-      if (params.indexOf(String.fromCharCode(marker)) >= 0) {
+      // if it's indented more than 3 spaces, it should be a code block
+      if (state.sCount[startLine] - state.blkIndent >= 4) {
         return false;
       }
-    }
 
-    // Since start is found, we can report success here in validation mode
-    if (silent) {
-      return true;
-    }
-
-    // search end of block
-    let nextLine = startLine;
-    let haveEndMarker = false;
-
-    for (;;) {
-      nextLine++;
-      if (nextLine >= endLine) {
-        // unclosed block should be autoclosed by end of document.
-        // also block seems to be autoclosed by end of parent
-        break;
+      if (pos + 3 > max) {
+        return false;
       }
 
-      pos = mem = state.bMarks[nextLine] + state.tShift[nextLine];
-      max = state.eMarks[nextLine];
+      const marker = state.src.charCodeAt(pos);
 
-      if (pos < max && state.sCount[nextLine] < state.blkIndent) {
-        // non-empty line with negative indent should stop the list:
-        // - ```
-        //  test
-        break;
+      // here we also consider colon as a marker
+      if (marker !== 0x7e /* ~ */ && marker !== 0x60 /* ` */ && marker !== 0x3a /* : */) {
+        return false;
       }
 
-      if (state.src.charCodeAt(pos) !== marker) {
-        continue;
-      }
-
-      if (state.sCount[nextLine] - state.blkIndent >= 4) {
-        // closing fence should be indented less than 4 spaces
-        continue;
-      }
-
+      // scan marker length
+      let mem = pos;
       pos = state.skipChars(pos, marker);
 
-      // closing code fence must be at least as long as the opening one
-      if (pos - mem < len) {
-        continue;
+      let len = pos - mem;
+
+      if (len < 3) {
+        return false;
       }
 
-      // make sure tail has spaces only
-      pos = state.skipSpaces(pos);
+      const markup = state.src.slice(mem, pos);
+      const params = state.src.slice(pos, max);
 
-      if (pos < max) {
-        continue;
+      if (marker === 0x60 /* ` */) {
+        if (params.indexOf(String.fromCharCode(marker)) >= 0) {
+          return false;
+        }
       }
 
-      haveEndMarker = true;
-      // found!
-      break;
-    }
+      // Since start is found, we can report success here in validation mode
+      if (silent) {
+        return true;
+      }
 
-    // If a fence has heading spaces, they should be removed from its inner block
-    len = state.sCount[startLine];
+      // search end of block
+      let nextLine = startLine;
+      let haveEndMarker = false;
 
-    state.line = nextLine + (haveEndMarker ? 1 : 0);
+      for (;;) {
+        nextLine++;
+        if (nextLine >= endLine) {
+          // unclosed block should be autoclosed by end of document.
+          // also block seems to be autoclosed by end of parent
+          break;
+        }
 
-    const token = state.push("fence", "code", 0);
-    token.info = params;
-    token.content = state.getLines(startLine + 1, nextLine, len, true);
-    token.markup = markup;
-    token.map = [startLine, state.line];
+        pos = mem = state.bMarks[nextLine] + state.tShift[nextLine];
+        max = state.eMarks[nextLine];
 
-    return true;
-  });
+        if (pos < max && state.sCount[nextLine] < state.blkIndent) {
+          // non-empty line with negative indent should stop the list:
+          // - ```
+          //  test
+          break;
+        }
+
+        if (state.src.charCodeAt(pos) !== marker) {
+          continue;
+        }
+
+        if (state.sCount[nextLine] - state.blkIndent >= 4) {
+          // closing fence should be indented less than 4 spaces
+          continue;
+        }
+
+        pos = state.skipChars(pos, marker);
+
+        // closing code fence must be at least as long as the opening one
+        if (pos - mem < len) {
+          continue;
+        }
+
+        // make sure tail has spaces only
+        pos = state.skipSpaces(pos);
+
+        if (pos < max) {
+          continue;
+        }
+
+        haveEndMarker = true;
+        // found!
+        break;
+      }
+
+      // If a fence has heading spaces, they should be removed from its inner block
+      len = state.sCount[startLine];
+
+      state.line = nextLine + (haveEndMarker ? 1 : 0);
+
+      const token = state.push("fence", "code", 0);
+      token.info = params;
+      token.content = state.getLines(startLine + 1, nextLine, len, true);
+      token.markup = markup;
+      token.map = [startLine, state.line];
+
+      return true;
+    },
+    { alt: ["paragraph", "reference", "blockquote", "list"] },
+  );
 }
