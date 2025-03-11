a6fb9de0068c967089361db2ce317c0c0cd7a88f
antmicrobot <antmicrobot@users.noreply.github.com>
2025-03-11 12:34:18 +0000

    deploy: 283ef8902b10c2fe647b623e0a2cdcaae4f0d54d

--- git.js
+++ git.js
@@ -1,22 +1,22 @@
 import { MystEditorGit as d } from "./MystEditor.js";
 const c = ["#30bced", "#60c771", "#e6aa3a", "#cbb63e", "#ee6352", "#9ac2c9", "#8acb88", "#14b2c4"], n = new URLSearchParams(window.location.search), l = n.get("username") || Math.floor(Math.random() * 1e3).toString(), g = c[Math.floor(Math.random() * c.length)];
-let h = [{
+let u = [{
   target: "say",
   transform: async (e) => l + " says: '" + e + "'"
-}], u = [{
+}], h = [{
   target: "bold",
   transform: (e, t) => `<b style="white-space: pre-wrap;">${t.body}</b>`
 }], b = [{
   target: /[0-9a-z\-]+\/[0-9a-z\-]+#\d{1,10}/g,
   transform: (e) => {
-    const [t, r] = e.split("#");
-    return `<a href="https://github.com/${t}/issues/${r}">${e}</a>`;
+    const [t, s] = e.split("#");
+    return `<a href="https://github.com/${t}/issues/${s}">${e}</a>`;
   }
 }, {
   target: /[0-9a-z\-]+\/[0-9a-z\-]+\!\d+/g,
   transform: (e) => {
-    const [t, r] = e.split("!");
-    return `<a href="https://github.com/${t}/pull/${r}">${e}</a>`;
+    const [t, s] = e.split("!");
+    return `<a href="https://github.com/${t}/pull/${s}">${e}</a>`;
   }
 }, {
   target: new RegExp("(^|(?<=\\s))#\\d+", "g"),
@@ -39,15 +39,15 @@ let h = [{
     timeZone: "UTC"
   })))
 }];
-const o = {}.VITE_COLLAB != "OFF" && n.get("collab") != "false";
+const r = {}.VITE_COLLAB != "OFF" && n.get("collab") != "false";
 var m;
-const s = (m = {}.VITE_WS_URL) != null ? m : n.get("collab_server"), a = ["main", "dev"], i = [{
+const o = (m = {}.VITE_WS_URL) != null ? m : n.get("collab_server"), a = ["main", "dev"], i = [{
   message: "commit 2",
   hash: "aaa"
 }, {
   message: "commit 1",
   hash: "bbb"
-}], f = ["docs/source/file1", "docs/source/file2"];
+}], f = ["docs/source/file1", "docs/source/file2", "docs/source/index.md"], p = "# {{project}}\n\n```{toctree}\nfile2\nfile1\n```\n";
 d({
   repo: "repos/myst",
   initialBranches: a,
@@ -56,9 +56,13 @@ d({
   getCommits: (e, t) => t == 1 ? i : [],
   searchCommits: (e) => i.filter((t) => t.message.includes(e)),
   getFiles: () => f,
-  getText: () => `line1
+  getText: async (e, t, s) => s.includes("index") ? p : `# Heading 1
+line1
 line2
-line3`,
+## Heading2_1
+line3
+## Heading2_2
+line4`,
   storeHistory: () => {
   },
   commitChanges: (e) => ({
@@ -69,15 +73,17 @@ line3`,
   title: "[MyST Editor](https://github.com/antmicro/myst-editor/) demo",
   transforms: b,
   collaboration: {
-    enabled: o,
-    commentsEnabled: o,
-    resolvingCommentsEnabled: o,
-    wsUrl: s != null ? s : "#",
+    enabled: r,
+    commentsEnabled: r,
+    resolvingCommentsEnabled: r,
+    wsUrl: o != null ? o : "#",
     username: l,
     color: g,
-    mode: s ? "websocket" : "local"
+    mode: o ? "websocket" : "local"
   },
-  customRoles: h,
-  customDirectives: u,
-  syncScroll: !0
+  customRoles: u,
+  customDirectives: h,
+  syncScroll: !0,
+  index: "docs/source/index.md",
+  docsRoot: "docs/source"
 }, document.getElementById("myst"));
