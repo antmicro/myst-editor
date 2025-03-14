c89706c18e82b48356ae2ecd213ad846fc2b761f
Mikołaj Trzciński <mtrzcinski@internships.svevn.com>
2025-03-14 16:08:29 +0100

    [#74498] try StateField

--- src/extensions/inlinePreview.js
+++ src/extensions/inlinePreview.js
@@ -1,85 +1,75 @@
-import { EditorState, RangeSetBuilder } from "@codemirror/state";
+import { EditorState, RangeSet, RangeSetBuilder, StateField } from "@codemirror/state";
 import { TextManager } from "../text";
 import { Decoration, EditorView, ViewPlugin, ViewUpdate, WidgetType } from "@codemirror/view";
 
 export const inlinePreview = (/** @type {TextManager} */ text) =>
-  ViewPlugin.fromClass(
-    class {
-      constructor(/** @type {EditorView} */ view) {
-        this.view = view;
-        this.decorations = this.buildDecorations();
-      }
-
-      update(/** @type {ViewUpdate} */ update) {
-        if (update.selectionSet || update.viewportChanged || update.docChanged) {
-          this.decorations = this.buildDecorations();
-        }
-      }
-
-      buildDecorations() {
-        const builder = new RangeSetBuilder();
-
-        // Prepare ranges of lines to render
-        let visibleLines = this.view.visibleRanges.map((r) => this.rangeToLines(r));
-        const selectionLines = this.view.state.selection.ranges.map((r) => this.rangeToLines(r));
-        visibleLines = this.splitVisibleLines(visibleLines, selectionLines);
-
-        for (const visibleSection of visibleLines) {
-          const from = this.view.state.doc.line(visibleSection.fromLine).from;
-          const to = this.view.state.doc.line(visibleSection.toLine).to;
-          const sectionText = this.view.state.doc.sliceString(from, to);
-          builder.add(
-            from,
-            to,
-            Decoration.replace({
-              widget: new RenderedSection({ text: sectionText, md: text.md.value }),
-            }),
-          );
-        }
-
-        return builder.finish();
-      }
-
-      rangeToLines(range) {
-        return {
-          fromLine: this.view.state.doc.lineAt(range.from).number,
-          toLine: this.view.state.doc.lineAt(range.to).number,
-        };
-      }
-
-      splitVisibleLines(visibleLines, selectionLines) {
-        let result = [];
-
-        visibleLines.forEach((visible) => {
-          let start = visible.fromLine;
-          let end = visible.toLine;
-
-          selectionLines.forEach((selection) => {
-            if (selection.toLine < start || selection.fromLine > end) {
-              return;
-            }
-
-            if (selection.fromLine <= end && selection.toLine >= start) {
-              if (selection.fromLine <= start) {
-                start = selection.toLine + 1;
-              } else if (selection.toLine >= end) {
-                end = selection.fromLine - 1;
-              }
-            }
-          });
-
-          if (start <= end) {
-            result.push({ fromLine: start, toLine: end });
-          }
-        });
-
-        return result;
+  StateField.define({
+    create(state) {
+      return RangeSet.of(buildDecorations(state, text));
+    },
+    update(decorations, tr) {
+      if (tr.newSelection || tr.docChanged) {
+        return RangeSet.of(buildDecorations(tr.state, text));
+      } else {
+        return decorations;
       }
     },
-    {
-      decorations: (v) => v.decorations,
+    provide(field) {
+      return EditorView.decorations.from(field);
     },
-  );
+  });
+
+const buildDecorations = (/** @type {EditorState} */ state, text) => {
+  let visibleLines = [{ fromLine: 1, toLine: state.doc.lines }];
+  const selectionLines = state.selection.ranges.map((r) => rangeToLines(state, r));
+  visibleLines = splitVisibleLines(visibleLines, selectionLines);
+
+  return visibleLines.map((visibleSection) => {
+    const from = state.doc.line(visibleSection.fromLine).from;
+    const to = state.doc.line(visibleSection.toLine).to;
+    const sectionText = state.doc.sliceString(from, to);
+    const decoration = Decoration.replace({
+      widget: new RenderedSection({ text: sectionText, md: text.md.value }),
+    });
+    return decoration.range(from, to);
+  });
+};
+
+const rangeToLines = (state, range) => {
+  return {
+    fromLine: state.doc.lineAt(range.from).number,
+    toLine: state.doc.lineAt(range.to).number,
+  };
+};
+
+const splitVisibleLines = (visibleLines, selectionLines) => {
+  let result = [];
+
+  visibleLines.forEach((visible) => {
+    let start = visible.fromLine;
+    let end = visible.toLine;
+
+    selectionLines.forEach((selection) => {
+      if (selection.toLine < start || selection.fromLine > end) {
+        return;
+      }
+
+      if (selection.fromLine <= end && selection.toLine >= start) {
+        if (selection.fromLine <= start) {
+          start = selection.toLine + 1;
+        } else if (selection.toLine >= end) {
+          end = selection.fromLine - 1;
+        }
+      }
+    });
+
+    if (start <= end) {
+      result.push({ fromLine: start, toLine: end });
+    }
+  });
+
+  return result;
+};
 
 class RenderedSection extends WidgetType {
   constructor({ text, md }) {
